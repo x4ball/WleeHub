53bf482db9b9b390b82e49cee3623f2aaf2bf4c0
x4ball <fayrixt@gmail.com>
2025-09-02 01:33:32 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,10 +1,11 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 local Window = WindUI:CreateWindow({
-    Title = "My Super Hub ",
-    Icon = "door-open",
+    Title = "WleeHub",
+    Icon = "laptop-minimal",
     Author = "by .ftgs and .ftgs",
     Folder = "MySuperHub",
     
+
     Size = UDim2.fromOffset(580, 460),
     Transparent = true,
     Theme = "Dark",
@@ -20,4 +21,22 @@ local Window = WindUI:CreateWindow({
             print("clicked")
         end,
     },
+})
+Window:Tag({
+    Title = "Working",
+    Color = Color3.fromHex("#30ff6a")
+})
+Window:Tag({
+    Title = "Beta Version",
+    Color = Color3.fromHex("FFEEF106")
+})
+local MainSection = Window:Section({
+    Title = "Section for the tabs",
+    Icon = "bird",
+    Opened = false,
+})
+MainSection:Tab({
+    Title = "ChangeLog",
+    Icon = "square-pen",
+    Locked = false,
 })
